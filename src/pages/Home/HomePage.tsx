d2738c0d3d78c9a5fe3d5651aef408b916343a22
Pellehakman <83712876+Pellehakman@users.noreply.github.com>
2023-12-21 17:21:15 +0100

    2

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -4,12 +4,12 @@ import Bar from "../../components/common/Info/Bar";
 
 function HomePage() {
   return (
-    <>
+    <div className="grow">
       <Bar />
       <h1>HomePage</h1>
       <Header />
       <NavLink to={"/products"}>product</NavLink>
-    </>
+    </div>
   );
 }
 
